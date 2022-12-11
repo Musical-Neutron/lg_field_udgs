4b98976337e8a9e93fa346a0b5a7a57fa7cd0817
Oliver Newton <oliver.newton.92@gmail.com>
2022-12-11 23:23:34 +0100

    Adding script to run all relevant scripts

---
+++ run_all_scripts.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+# Place import files below
+import generate_paper_results
+import fig_01_plot_selection_criteria
+import fig_03_plot_cumulative_rad_dist
+import fig_04_plot_luminosity_functions
+import fig_05_plot_mock_sdss_observations
+import print_paper_results
+
+
+def main():
+    # Generate paper data
+    print("Generating paper data")
+    generate_paper_results.main()
+
+    # Plot Fig. 1
+    fig_01_plot_selection_criteria.main()
+
+    # Plot Fig. 3
+    fig_03_plot_cumulative_rad_dist.main()
+
+    # Plot Fig. 4
+    fig_04_plot_luminosity_functions.main()
+
+    # Plot Fig. 5
+    fig_05_plot_mock_sdss_observations.main()
+
+    # Print paper results
+    print_paper_results.main()
+    return None
+
+
+if __name__ == "__main__":
+    main()
